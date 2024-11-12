0d8a7da55da7affe7f312edf62a0cc053f8031f1
JHoldsworth23 <jacobph101@icloud.com>
2024-11-12 19:03:02 +0000

    add the example data in personal experience and education for the cv

---
+++ src/example-data.js
@@ -0,0 +1,29 @@
+export const exampleData = {
+    personal: {
+        fullName: 'Harry Du Bois',
+        phoneNo: '+44 7843 348294',
+        email: 'ummmwhatsemail@gmail.com',
+        address: 'London, UK',
+    },
+    sections: {
+        experience: [
+            {
+                company: 'WarioWare Inc.',
+                role: 'Software Engineering',
+                location: 'Diamond City',
+                description: 'Developed and deployed robust software solutions, collaborating with cross-functional teams to meet business requirements and optimize performance. Led the design and implementation of scalable features, utilizing best practices in coding, testing, and version control to ensure high-quality software delivery.',
+                startDate: '10/2023',
+                endDate: '06/2024',
+            }
+        ],
+        education: [
+            {
+                school: 'University of London',
+                study: 'BSc Computer Science',
+                location: 'London, UK',
+                startDate: '09/2022',
+                endDate: 'Present',
+            }
+        ],
+    }
+}
