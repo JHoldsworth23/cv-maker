bda15eef46361150d82206b2442d6b0bddcfa639
JHoldsworth23 <jacobph101@icloud.com>
2024-11-19 19:45:27 +0000

    add styles for the experience form section

--- src/components/experience/experience-form-section.jsx
+++ src/components/experience/experience-form-section.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 import DisplayForm from "../display-form";
 import ExperienceForm from "./experience-form";
+import "../../styles/form-section.css";
 
 export default function ExperienceFormSection({ experiences, onChange }) {
     return (
-        <div className="experience-form-section">
-            <h2>Experience</h2>
+        <div className="experience-form-section section">
+            <h2 className="section-heading">Experience</h2>
             <div className="experience-forms">
                 <DisplayForm 
                   forms={experiences}
