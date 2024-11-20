087109c82bbea04bdff5a07d1a5edffae651e535
JHoldsworth23 <jacobph101@icloud.com>
2024-11-20 20:06:33 +0000

    add props section name and onClick for button

--- src/components/collapsed-form.jsx
+++ src/components/collapsed-form.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
 import "../styles/collapsed-form.css";
 
-export default function CollapsedForm({ id, title }) {
+export default function CollapsedForm({ id, title, sectionName, onClick }) {
     return (
-        <button className="collapsed-form" id={id}>
+        <button 
+          className="collapsed-form" 
+          id={id}
+          onClick={onClick}
+          data-section-name={sectionName}
+        >
             <p>{title}</p>
         </button>
     );
