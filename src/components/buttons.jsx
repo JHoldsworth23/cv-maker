4ecacb879e6f02fca5dbd102a384878b237884e3
JHoldsworth23 <jacobph101@icloud.com>
2024-11-24 14:53:29 +0000

    add cancel button and style and adjust position of buttons

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -2,14 +2,14 @@
 export default function Buttons({ save, remove, cancel }) {
     return (
         <div className="buttons">
+            <button className="delete-btn btn" onClick={remove} type="button">
+                Delete
+            </button>
             <button className="cancel-btn btn" onClick={cancel} type="button">
                 Cancel
             </button>
             <button className="save-btn btn" onClick={save} type="submit">
                 Save
-            </button>
-            <button className="delete-btn btn" onClick={remove} type="button">
-                Delete
             </button>
         </div>
     );
