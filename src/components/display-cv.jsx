7c7e88a1a09ecdeafa179efd1aaae22d66f8d294
JHoldsworth23 <jacobph101@icloud.com>
2024-11-28 21:01:39 +0000

    import cv-section component for experience and education

--- src/components/display-cv.jsx
+++ src/components/display-cv.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
 import PersonalSection from "./personal/personal-section";
+import CVSection from "./cv-section";
 import "../styles/cv-display.css";
 
 export default function DisplayCV({ personal, sections }) {
     return (
         <div className="cv-display">
             <PersonalSection information={personal} />
-            {/* Experience Section */}
-            {/* Education Section */}
+            <div>
+                <CVSection title="Experience" section={sections.experience} />
+                <CVSection title="Education" section={sections.education} />
+            </div>
         </div>
     );
 }
