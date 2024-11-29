086e85bf8545a43248a2285b03f325ed6b656ace
JHoldsworth23 <jacobph101@icloud.com>
2024-11-29 21:02:07 +0000

    add class name to display information

--- src/components/display-cv.jsx
+++ src/components/display-cv.jsx
@@ -7,8 +7,8 @@ export default function DisplayCV({ personal, sections }) {
     return (
         <div className="cv-display">
             <PersonalSection information={personal} />
-            <div>
-                <CVSection title="Experience" section={sections.experience} />
+            <div className="information-display">
+                <CVSection title="Professional Experience" section={sections.experience} />
                 <CVSection title="Education" section={sections.education} />
             </div>
         </div>
