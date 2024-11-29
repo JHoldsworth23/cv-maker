4742819d8ca496e38bdbb0c70360950381ad6dee
JHoldsworth23 <jacobph101@icloud.com>
2024-11-29 21:01:42 +0000

    add class names and use method to check if the string is matched

--- src/components/cv-section.jsx
+++ src/components/cv-section.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable react/prop-types */
 import ExperienceSection from "./experience/experience-section";
 import EducationSection from "./education/education-section";
+import "../styles/cv-section.css";
 
 export default function CVSection({ title, section }) {
     return (
         <>
-            <div>
-                <h3>{title}</h3>
+            <div className="section-title-div">
+                <h3 className="section-title">{title}</h3>
             </div>
-            {title === "Experience" 
+            {title.match("Experience")
                 ? section.map(experience => 
-                    <div key={experience.id}>
+                    <div key={experience.id} className="experience-section">
                         <ExperienceSection {...experience} />
                     </div>
                 )
                 : section.map(education => 
-                    <div key={education.id}>
+                    <div key={education.id} className="education-section">
                         <EducationSection {...education} />
                     </div>
                 )}
