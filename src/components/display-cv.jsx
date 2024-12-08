ca531f4c66fab9aa78aa82723abf0d5a02ad673f
JHoldsworth23 <jacobph101@icloud.com>
2024-12-08 20:55:55 +0000

    add function not to display section header when all information is hidden

--- src/components/display-cv.jsx
+++ src/components/display-cv.jsx
@@ -6,12 +6,18 @@ import "../styles/cv-display.css";
 export default function DisplayCV({ personal, sections, layout }) {
     const { experience, education } = sections;
 
+    function showInformation(array) {
+        return array
+            .map(obj => {return obj.isHidden})
+            .some((value) => value === false);
+    }
+
     return (
         <div className={`cv-display ${layout}`}>
             <PersonalSection information={personal} />
             <div className="information-display">
-                {experience.length > 0 && <CVSection title="Professional Experience" section={experience} />}
-                {education.length > 0 && <CVSection title="Education" section={education} />}
+                {experience.length > 0 && showInformation(experience) && <CVSection title="Professional Experience" section={experience} />}
+                {education.length > 0 && showInformation(education) && <CVSection title="Education" section={education} />}
             </div>
         </div>
     );
